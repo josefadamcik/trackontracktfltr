45b936aa8a82810833270845fe89c93c632b0a93
Josef Adamcik <josef.adamcik@gmail.com>
2018-05-30 18:09:58 +0200

    Fixed test and hopefully even Travis build.

--- lib/trakt_api.dart
+++ lib/trakt_api.dart
@@ -1,6 +1,6 @@
-import 'dart:_http';
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
 import 'package:trackontraktfltr/models/history_item.dart';
